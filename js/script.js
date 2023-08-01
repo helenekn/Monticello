8203ae61610a0bdf6c651162015b6d4fbc110625
Helen <redkina2508@gmail.com>
2023-08-01 17:25:00 +0200

    added modal

--- js/script.js
+++ js/script.js
@@ -16,8 +16,23 @@ window.addEventListener("DOMContentLoaded", () => {
    });
 });
 
-// let btn = document.querySelector(".content__btn");
+let btns = document.querySelectorAll(".content__btn"),
+   modal_1 = document.querySelector(".modal_first"),
+   modal_2 = document.querySelector(".modal_second"),
+   btns_modal = document.querySelectorAll(".modal__btn");
 
-// btn.addEventListener('click', () => {
+btns[0].addEventListener("click", () => {
+   modal_1.classList.toggle("modal_active");
+});
 
-// })
+btns[1].addEventListener("click", () => {
+   modal_2.classList.toggle("modal_active");
+});
+
+btns_modal[0].addEventListener("click", () => {
+   modal_1.classList.toggle("modal_active");
+});
+
+btns_modal[1].addEventListener("click", () => {
+   modal_2.classList.toggle("modal_active");
+});
