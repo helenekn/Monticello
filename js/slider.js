30c930657f26bf826275354699afc1147f52432c
Helen <redkina2508@gmail.com>
2023-08-01 13:40:07 +0200

    first commit

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+   $(".slider").slick({
+      dots: true,
+      arrows: false,
+
+      autoplay: true,
+      autoplaySpeed: 4500,
+      speed: 1500,
+      slidesToShow: 1,
+      infinite: true,
+      swipe: true,
+      fade: true,
+   });
+});
+
+$(document).ready(function () {
+   $(".slider-news").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+      dots: true,
+      autoplay: true,
+   });
+});
